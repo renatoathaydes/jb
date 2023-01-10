cf2995e112f5bb689981c84af02c1b172f493413
Renato Athaydes <renato@athaydes.com>
2023-01-10 22:59:39 +0100

    Simplified RPC args so it's strongly typed.

--- lib/src/jvm_executor.dart
+++ lib/src/jvm_executor.dart
@@ -45,7 +45,8 @@ class JvmExecutor {
         message: 'JBuild RPC returned unexpected value: $value');
   }
 
-  Future<dynamic> run(String? className, String methodName, List args,
+  Future<dynamic> run(
+      String? className, String methodName, List<List<String>> args,
       {Map<String, String> env = const {}}) async {
     final process = await _startOrGetProcess(env);
     final client = HttpClient();
@@ -121,7 +122,7 @@ class JvmExecutor {
   }
 
   List<int> _createRpcMessage(
-      String? className, String methodName, List<dynamic> args) {
+      String? className, String methodName, List<List<String>> args) {
     final method = className == null ? methodName : '$className.$methodName';
     final message = '<?xml version="1.0"?>'
         '<methodCall>'
@@ -132,133 +133,122 @@ class JvmExecutor {
     return utf8.encode(message);
   }
 
-  String _rpcParams(List<dynamic> args) {
+  String _rpcParams(List<List<String>> args) {
     return args.map(_rpcParam).join();
   }
 
-  String _rpcParam(dynamic arg) {
-    return '<param>${_rpcValue(arg)}</param>';
+  String _rpcParam(List<String> arg) {
+    return '<param>${_rpcValues(arg)}</param>';
   }
 
-  String _rpcValue(dynamic arg) {
-    if (arg is String) {
-      return '<value><string>$arg</string></value>';
-    }
-    if (arg is Iterable<String>) {
-      return '<value><array><data>'
-          '${arg.map(_rpcValue).join()}'
-          '</data></array></value>';
-    }
-    if (arg is int) {
-      return '<value><i4>$arg</i4></value>';
-    }
-    if (arg is bool) {
-      return '<value><boolean>$arg</boolean></value>';
-    }
-    if (arg is double) {
-      return '<value><double>$arg</double></value>';
-    }
-    throw ArgumentError('type is not supported in RPC: ${arg.runtimeType}');
+  String _rpcValue(String arg) {
+    return '<value><string>$arg</string></value>';
   }
 
-  Future<dynamic> _parseRpcResponse(Stream<List<int>> rpcResponse) async {
-    final message = await rpcResponse.text();
-    logger.fine(() => 'Received RPC response: $message');
-    final XmlDocument doc;
-    try {
-      doc = XmlDocument.parse(message);
-      // await data.transform(ChunkDecoder()).transform(utf8.decoder).first);
-    } on XmlException catch (e) {
-      throw DartleException(message: 'RPC response could not be parsed: $e');
-    }
-    final response = doc
-        .getElement('methodResponse')
-        .orThrow('no methodResponse in RPC response:\n'
-            '${doc.toXmlString(pretty: true)}');
+  String _rpcValues(List<String> arg) {
+    return '<value><array><data>'
+        '${arg.map(_rpcValue).join()}'
+        '</data></array></value>';
+  }
+}
 
-    final fault = response.getElement('fault');
-    if (fault != null) {
-      return _rpcFault(fault);
-    }
+Future<dynamic> _parseRpcResponse(Stream<List<int>> rpcResponse) async {
+  final message = await rpcResponse.text();
+  logger.fine(() => 'Received RPC response: $message');
+  final XmlDocument doc;
+  try {
+    doc = XmlDocument.parse(message);
+    // await data.transform(ChunkDecoder()).transform(utf8.decoder).first);
+  } on XmlException catch (e) {
+    throw DartleException(message: 'RPC response could not be parsed: $e');
+  }
+  final response = doc
+      .getElement('methodResponse')
+      .orThrow('no methodResponse in RPC response:\n'
+          '${doc.toXmlString(pretty: true)}');
 
-    final params = response.getElement('params').orThrowA(
-        () => 'RPC response missing params:\n${doc.toXmlString(pretty: true)}');
+  final fault = response.getElement('fault');
+  if (fault != null) {
+    return _rpcFault(fault);
+  }
 
-    // params may be empty or contain one result
-    final paramList = params.findElements('param').toList(growable: false);
-    if (paramList.isEmpty) return null;
-    if (paramList.length == 1) {
-      return _value(paramList[0]);
-    }
+  final params = response.getElement('params').orThrowA(
+      () => 'RPC response missing params:\n${doc.toXmlString(pretty: true)}');
+
+  // params may be empty or contain one result
+  final paramList = params.findElements('param').toList(growable: false);
+  if (paramList.isEmpty) return null;
+  if (paramList.length == 1) {
+    return _value(paramList[0]);
+  }
+  throw DartleException(
+      message: 'RPC response contains multiple parameters, '
+          'which is not supported: ${doc.toXmlString(pretty: true)}');
+}
+
+dynamic _value(XmlElement element) {
+  final value = element.getElement('value');
+  if (value == null) return null;
+  final children = value.children;
+  if (children.length != 1) {
     throw DartleException(
-        message: 'RPC response contains multiple parameters, '
-            'which is not supported: ${doc.toXmlString(pretty: true)}');
+        message: 'RPC value contains too many children: '
+            '${value.toXmlString(pretty: true)}');
   }
-
-  dynamic _value(XmlElement element) {
-    final value = element.getElement('value');
-    if (value == null) return null;
-    final children = value.children;
-    if (children.length != 1) {
+  final child = children.first as XmlElement;
+  switch (child.localName) {
+    case 'string':
+      return child.text;
+    case 'int':
+    case 'i4':
+      return int.parse(child.text);
+    case 'double':
+      return double.parse(child.text);
+    case 'boolean':
+      return child.text == '1';
+    default:
       throw DartleException(
-          message: 'RPC value contains too many children: '
-              '${value.toXmlString(pretty: true)}');
-    }
-    final child = children.first as XmlElement;
-    switch (child.localName) {
-      case 'string':
-        return child.text;
-      case 'int':
-      case 'i4':
-        return int.parse(child.text);
-      case 'double':
-        return double.parse(child.text);
-      case 'boolean':
-        return child.text == '1';
-      default:
-        throw DartleException(
-            message: 'RPC value type not supported: ${child.nodeType.name}');
-    }
+          message: 'RPC value type not supported: ${child.nodeType.name}');
   }
+}
 
-  Future<Never> _rpcFault(XmlElement fault) async {
-    final struct = fault
-        .getElement('value')
-        .orThrowA(() =>
-            'RPC fault missing value:\n${fault.toXmlString(pretty: true)}')
-        .getElement('struct')
-        .orThrowA(() =>
-            'RPC fault missing struct:\n${fault.toXmlString(pretty: true)}');
+Future<Never> _rpcFault(XmlElement fault) async {
+  final struct = fault
+      .getElement('value')
+      .orThrowA(
+          () => 'RPC fault missing value:\n${fault.toXmlString(pretty: true)}')
+      .getElement('struct')
+      .orThrowA(() =>
+          'RPC fault missing struct:\n${fault.toXmlString(pretty: true)}');
 
-    final members = struct.findElements('member');
-    final faultString = members
-        .firstWhere((m) => m.getElement('name')?.text == 'faultString',
-            orElse: () => throw DartleException(
-                message: 'RPC fault missing faultString:\n'
-                    '${struct.toXmlString(pretty: true)}'))
-        .getElement('value')
-        .orThrowA(() => 'RPC faultString missing value:\n'
-            '${struct.toXmlString(pretty: true)}')
-        .getElement('string')
-        .orThrowA(() => 'RPC faultString missing string value:\n'
-            '${struct.toXmlString(pretty: true)}')
-        .text;
+  final members = struct.findElements('member');
+  final faultString = members
+      .firstWhere((m) => m.getElement('name')?.text == 'faultString',
+          orElse: () => throw DartleException(
+              message: 'RPC fault missing faultString:\n'
+                  '${struct.toXmlString(pretty: true)}'))
+      .getElement('value')
+      .orThrowA(() => 'RPC faultString missing value:\n'
+          '${struct.toXmlString(pretty: true)}')
+      .getElement('string')
+      .orThrowA(() => 'RPC faultString missing string value:\n'
+          '${struct.toXmlString(pretty: true)}')
+      .text;
 
-    final faultCode = int.parse(members
-        .firstWhere((m) => m.getElement('name')?.text == 'faultCode',
-            orElse: () => throw DartleException(
-                message: 'RPC faultCode missing:\n'
-                    '${struct.toXmlString(pretty: true)}'))
-        .getElement('value')
-        .orThrowA(() => 'RPC faultCode missing value:\n'
-            '${struct.toXmlString(pretty: true)}')
-        .getElement('int')
-        .orThrowA(() => 'RPC faultCode missing int value:\n'
-            '${struct.toXmlString(pretty: true)}')
-        .text);
+  final faultCode = int.parse(members
+      .firstWhere((m) => m.getElement('name')?.text == 'faultCode',
+          orElse: () => throw DartleException(
+              message: 'RPC faultCode missing:\n'
+                  '${struct.toXmlString(pretty: true)}'))
+      .getElement('value')
+      .orThrowA(() => 'RPC faultCode missing value:\n'
+          '${struct.toXmlString(pretty: true)}')
+      .getElement('int')
+      .orThrowA(() => 'RPC faultCode missing int value:\n'
+          '${struct.toXmlString(pretty: true)}')
+      .text);
 
-    throw DartleException(message: faultString, exitCode: faultCode);
-  }
+  throw DartleException(message: faultString, exitCode: faultCode);
 }
 
 Future<void> _sendStopMessage(int port, String authorizationHeader) async {
