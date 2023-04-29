450690394d6b26d3a87e125fde205af283c50d04
Renato Athaydes <renato@athaydes.com>
2023-04-29 16:17:24 +0200

    Test to verify output changes cause full recompilation.

--- test/incremental_compilation.dart
+++ test/incremental_compilation.dart
@@ -9,14 +9,14 @@ void main() {
   group('Incremental Compilation', () {
     test('can recompile only changed sources', () async {
       final rootDir = await createTempFiles({
-        'jbuild.yaml': 'source-dirs: [src]\noutput-dir: build/classes',
-        p.join('src', 'util', 'Util.java'): '''
+        'jbuild.yaml': 'source-dirs: [source]\noutput-dir: build/classes',
+        p.join('source', 'util', 'Util.java'): '''
         package util;
         public class Util {
           public static String greeting() { return "Hi"; }
         }
         ''',
-        p.join('src', 'Main.java'): '''
+        p.join('source', 'Main.java'): '''
         import util.Util;
         class Main {
           public static void main(String[] args) {
@@ -50,13 +50,13 @@ void main() {
 
       // change the Util class and delegate to a new class
       await createFiles(rootDir, {
-        p.join('src', 'util', 'Util.java'): '''
+        p.join('source', 'util', 'Util.java'): '''
         package util;
         public class Util {
           public static String greeting() { return MsgUtil.greeting(); }
         }
         ''',
-        p.join('src', 'util', 'MsgUtil.java'): '''
+        p.join('source', 'util', 'MsgUtil.java'): '''
         package util;
         public class MsgUtil {
           public static String greeting() { return "Hello"; }
@@ -98,12 +98,12 @@ void main() {
         () async {
       final rootDir = await createTempFiles({
         'jbuild.yaml': '''
-          source-dirs: [src]
+          source-dirs: [source]
           resource-dirs: [res]
           output-dir: build/classes
         ''',
         p.join('res', 'greeting.txt'): 'Hej',
-        p.join('src', 'util', 'Resources.java'): '''
+        p.join('source', 'util', 'Resources.java'): '''
         package util;
         import java.nio.charset.StandardCharsets;
         public class Resources {
@@ -116,7 +116,7 @@ void main() {
           }
         }
         ''',
-        p.join('src', 'util', 'Util.java'): '''
+        p.join('source', 'util', 'Util.java'): '''
         package util;
         public class Util {
           public static String greeting() { 
@@ -124,7 +124,7 @@ void main() {
           }
         }
         ''',
-        p.join('src', 'Main.java'): '''
+        p.join('source', 'Main.java'): '''
         import util.Util;
         class Main {
           public static void main(String[] args) {
@@ -159,11 +159,11 @@ void main() {
       expect(javaResult.stdout, contains('Hej World'));
 
       // delete the Resources class and the resource, update Utils class
-      await File(p.join(rootDir.path, 'src', 'util', 'Resources.java'))
+      await File(p.join(rootDir.path, 'source', 'util', 'Resources.java'))
           .delete();
       await File(p.join(rootDir.path, 'res', 'greeting.txt')).delete();
       await createFiles(rootDir, {
-        p.join('src', 'util', 'Util.java'): '''
+        p.join('source', 'util', 'Util.java'): '''
         package util;
         public class Util {
           public static String greeting() { return "Basic Hi"; }
@@ -198,6 +198,67 @@ void main() {
           equals(mainTimestamp.millisecondsSinceEpoch));
       expect(utilTimestamp2.millisecondsSinceEpoch,
           greaterThan(utilTimestamp.millisecondsSinceEpoch));
+    });
+
+    test('does not perform incremental compilation if output changes',
+        () async {
+      final rootDir = await createTempFiles({
+        'jbuild.yaml': 'source-dirs: [source]\noutput-dir: build/classes',
+        p.join('source', 'util', 'Util.java'): '''
+        package util;
+        public class Util {
+          public static String greeting() { return "Hi"; }
+        }
+        ''',
+        p.join('source', 'Main.java'): '''
+        import util.Util;
+        class Main {
+          public static void main(String[] args) {
+            System.out.println(Util.greeting() + " World");
+          }
+        }
+        ''',
+      });
+
+      final jbResult = await runJb(rootDir);
+      expectSuccess(jbResult);
+
+      await assertDirectoryContents(
+          Directory(p.join(rootDir.path, 'build', 'classes')), [
+        'Main.class',
+        'util',
+        p.join('util', 'Util.class'),
+      ]);
+
+      // change the output dir, which can cause an error if incremental
+      // compilation kicked in.
+      await File(p.join(rootDir.path, 'build', 'classes', 'util', 'Util.class'))
+          .delete();
+
+      final mainTimestamp =
+          await File(p.join(rootDir.path, 'build', 'classes', 'Main.class'))
+              .lastModified();
+
+      final jbResult2 = await runJb(rootDir);
+      expectSuccess(jbResult2);
+
+      await assertDirectoryContents(
+          Directory(p.join(rootDir.path, 'build', 'classes')), [
+        'Main.class',
+        'util',
+        p.join('util', 'Util.class'),
+      ]);
+
+      final javaResult2 =
+          await runJava(rootDir, ['-cp', p.join('build', 'classes'), 'Main']);
+      expectSuccess(javaResult2);
+      expect(javaResult2.stdout, contains('Hi World'));
+
+      final mainTimestamp2 =
+          await File(p.join(rootDir.path, 'build', 'classes', 'Main.class'))
+              .lastModified();
+      expect(mainTimestamp2.millisecondsSinceEpoch,
+          greaterThan(mainTimestamp.millisecondsSinceEpoch));
     });
   });
 }
