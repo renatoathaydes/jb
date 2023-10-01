65b974847795b67ce2a3fa42a327b3c40ec9ecb3
Renato Athaydes <renato@athaydes.com>
2023-10-01 14:59:58 +0200

    Created licenses generation Dartle task.
    Generate SPDX licenses so jb projects can pick licenses.

--- dartle.dart
+++ dartle.dart
@@ -1,11 +1,12 @@
 import 'package:dartle/dartle_dart.dart';
 
 import 'dartle-src/clean_tests.dart' as cleaners;
+import 'dartle-src/distribution.dart' as dist;
 import 'dartle-src/empty_generated_assets.dart' as emptier;
 import 'dartle-src/generate_embedded_assets.dart' as comp;
-import 'dartle-src/setup_test_mvn_repo.dart' as tests;
-import 'dartle-src/distribution.dart' as dist;
+import 'dartle-src/generate_licenses.dart' as lic;
 import 'dartle-src/generate_version_file.dart' as gen;
+import 'dartle-src/setup_test_mvn_repo.dart' as tests;
 
 final dartleDart = DartleDart();
 
@@ -15,9 +16,11 @@ void main(List<String> args) async {
   cleaners.setupTaskDependencies(dartleDart);
   dist.setupTaskDependencies(dartleDart);
   gen.setupTaskDependencies(dartleDart);
+  lic.setupTaskDependencies(dartleDart);
 
   await run(args, tasks: {
     comp.generateEmbeddedAssetsTask,
+    lic.generateLicensesTask,
     tests.buildMvnRepoListsProjectTask,
     tests.setupTestMvnRepoTask,
     emptier.emptyGeneratedAssetsTask,
