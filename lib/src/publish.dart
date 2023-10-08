3c8797d6b24885e17c87386d39809c9d8aa0f847
Renato Athaydes <renato@athaydes.com>
2023-10-08 19:58:14 +0200

    Added PROFILE logs to publishing steps, added bundle.jar creation
    prior to publishing artifacts to HTTP repository (which is not implemented yet).

--- lib/src/publish.dart
+++ lib/src/publish.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:conveniently/conveniently.dart';
 import 'package:crypto/crypto.dart';
 import 'package:dartle/dartle.dart'
-    show ArgsCount, DartleException, homeDir, failBuild, execProc, profile;
+    show ArgsCount, homeDir, failBuild, execProc, profile, tempDir, tempFile;
 import 'package:path/path.dart' as p;
 
 import 'config.dart';
@@ -32,28 +32,48 @@ class Publisher {
     final destination =
         args.isEmpty ? p.join(home, '.m2', 'repository') : args[0];
 
+    final stopwatch = Stopwatch()..start();
+
     if (destination == '-m') {
-      return await _publishHttp(_mavenRepo);
+      return await _publishHttp(_mavenRepo, stopwatch);
     }
+
     if (destination.startsWith('http://') ||
         destination.startsWith('https://')) {
-      return await _publishHttp(destination);
+      return await _publishHttp(destination, stopwatch);
     }
-    await _publishLocal(destination);
+    await _publishLocal(destination, stopwatch);
   }
 
-  Future<void> _publishLocal(String repoPath) async {
+  Artifact _getArtifact() {
     final theArtifact = switch (artifact) {
       Ok(value: var a) => a,
       Fail(exception: var e) => throw e,
     };
-
-    final destination = Directory(_pathFor(theArtifact, repoPath));
-    await _putPublicationOn(destination, theArtifact);
+    return theArtifact;
   }
 
-  Future<void> _putPublicationOn(
-      Directory destination, Artifact artifact) async {
+  Future<void> _publishLocal(String repoPath, Stopwatch stopwatch) async {
+    final theArtifact = _getArtifact();
+
+    final destination = Directory(_pathFor(theArtifact, repoPath));
+    logger.info(() => 'Publishing artifacts to ${destination.path}');
+    await _publishArtifactToDir(destination, theArtifact, stopwatch);
+  }
+
+  Future<void> _publishHttp(String repoUri, Stopwatch stopwatch) async {
+    final destination = tempDir(suffix: '-jb-publish');
+    logger.fine(() => 'Creating publication artifacts at ${destination.path}');
+    await _publishArtifactToDir(destination, _getArtifact(), stopwatch);
+    logger.fine(() => 'Creating bundle.jar with publication artifacts');
+    final bundle = await _createBundleJar(destination, stopwatch);
+    logger.info('Bundle created at ${bundle.path}, cannot yet publish it');
+  }
+
+  Future<void> _publishArtifactToDir(
+      Directory destination, Artifact artifact, Stopwatch stopwatch) async {
+    stopwatch.reset();
+
     // ignore error intentionally
     await catching(() => destination.delete(recursive: true));
     // create an empty directory
@@ -62,15 +82,22 @@ class Publisher {
     // so this should never fail
     final jarFile = jar.ifBlank(() => 'Cannot publish, jar was not provided');
 
-    logger.info(() => 'Publishing artifacts to ${destination.path}');
-
-    final stopwatch = Stopwatch()..start();
-
     final pom = createPom(artifact, dependencies, localDependencies);
 
     logger.log(
         profile, () => 'Created POM in ${stopwatch.elapsedMilliseconds} ms');
     stopwatch.reset();
+
+    await _publishFiles(destination, artifact, pom, jarFile);
+
+    logger.log(
+        profile,
+        () => 'Created publication artifacts in '
+            '${stopwatch.elapsedMilliseconds} ms');
+  }
+
+  Future<void> _publishFiles(Directory destination, Artifact artifact,
+      StringBuffer pom, String jarFile) async {
     await File(p.join(destination.path, _fileFor(artifact, extension: '.pom')))
         .writeAsString(pom.toString())
         .then(_signFile)
@@ -89,23 +116,29 @@ class Publisher {
             p.join(destination.path, _fileFor(artifact, qualifier: '-javadoc')))
         .then(_signFile)
         .then(_shaFile);
-    logger.log(
-        profile,
-        () => 'Created publication artifacts in '
-            '${stopwatch.elapsedMilliseconds} ms');
   }
 
-  Future<void> _publishHttp(String repoUri) async {
-    throw DartleException(
-        message: 'Publishing to HTTP repositories is not supported yet');
+  Future<File> _createBundleJar(
+      Directory directory, Stopwatch stopwatch) async {
+    // jar --create --file target/bundle.jar -C target/deploy .
+    final bundleJar = tempFile(extension: '.jar');
+    logger.fine(() => 'Creating publication bundle jar at ${bundleJar.path}');
+    stopwatch.reset();
+    await execProc(Process.start('jar',
+        ['--create', '--file', bundleJar.path, '-C', directory.path, '.']));
+    logger.log(
+        profile,
+        () => 'Created publication bundle jar at ${bundleJar.path} in '
+            '${stopwatch.elapsedMilliseconds} ms.');
+    return bundleJar;
   }
 }
 
 Future<void> _shaFile(File file) async {
-  logger.finer(() => 'Computing SHA1 of $file');
+  logger.finer(() => 'Computing SHA1 of ${file.path}');
   await File('${file.path}.sha1')
       .writeAsString(sha1.convert(await file.readAsBytes()).toString());
-  logger.finer(() => 'Computed SHA1 of $file');
+  logger.finer(() => 'Computed SHA1 of ${file.path}');
 }
 
 // remember if GPG fails so we don't try again
