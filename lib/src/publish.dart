330d4d712fab4bc7a0a2128ce4adfcacc4fd6b1b
Renato Athaydes <renato@athaydes.com>
2023-10-25 20:43:06 +0200

    Publish jb project on HTTP Maven repositories.

--- lib/src/publish.dart
+++ lib/src/publish.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:conveniently/conveniently.dart';
@@ -13,7 +14,9 @@ import 'resolved_dependency.dart';
 import 'utils.dart';
 
 // See https://central.sonatype.org/publish/publish-manual/#close-and-drop-or-release-your-staging-repository
-const _mavenRepo = 'https://s01.oss.sonatype.org/service/local';
+const _mavenCentralUrl = 'https://s01.oss.sonatype.org/service/local';
+
+const _successCodes = {200, 201, 202, 203, 204};
 
 class Publisher {
   static final argsValidator = ArgsCount.range(min: 0, max: 1);
@@ -35,7 +38,7 @@ class Publisher {
     final stopwatch = Stopwatch()..start();
 
     if (destination == '-m') {
-      return await _publishHttp(_mavenRepo, stopwatch);
+      return await _publishHttp(_mavenCentralUrl, stopwatch);
     }
 
     if (destination.startsWith('http://') ||
@@ -62,12 +65,24 @@ class Publisher {
   }
 
   Future<void> _publishHttp(String repoUri, Stopwatch stopwatch) async {
+    Uri uri;
+    try {
+      uri = Uri.parse(repoUri);
+    } catch (e) {
+      failBuild(reason: 'Repository URI is invalid: "$repoUri"');
+    }
     final destination = tempDir(suffix: '-jb-publish');
     logger.fine(() => 'Creating publication artifacts at ${destination.path}');
     await _publishArtifactToDir(destination, _getArtifact(), stopwatch);
     logger.fine(() => 'Creating bundle.jar with publication artifacts');
     final bundle = await _createBundleJar(destination, stopwatch);
-    logger.info('Bundle created at ${bundle.path}, cannot yet publish it');
+    stopwatch.reset();
+    logger.log(
+        profile,
+        () => 'Publication bundle created at ${bundle.path} in '
+            '${stopwatch.elapsedMilliseconds} ms');
+    logger.info(() => 'Publishing artifacts to $repoUri');
+    await _httpPost(uri, bundle, _mavenCredentials());
   }
 
   Future<void> _publishArtifactToDir(
@@ -132,6 +147,78 @@ class Publisher {
             '${stopwatch.elapsedMilliseconds} ms.');
     return bundleJar;
   }
+
+  Future<void> _httpPost(
+      Uri repoUri, File bundle, HttpClientBasicCredentials? credentials) async {
+    final client = HttpClient();
+    if (credentials != null) {
+      await _login(repoUri, client, credentials);
+    }
+    final uploadRequest =
+        await client.postUrl(repoUri.resolve('staging/bundle_upload'));
+    await uploadRequest.addStream(bundle.openRead());
+    final uploadResponse = await uploadRequest.close();
+    await _verifySuccess(uploadResponse,
+        error: 'Failed to upload bundle to Maven repository');
+    final publicationIds = await _parseUploadResponse(uploadResponse);
+    if (publicationIds.isEmpty) {
+      logger.warning(() => 'The Maven repository did not return the staging ID '
+          'so the publication could not be finalized. '
+          'You must manually release from staging!');
+      return;
+    }
+    logger.fine('Closing publication');
+    final promoteRequest =
+        await client.postUrl(repoUri.resolve('staging/bulk/promote'));
+    promoteRequest.add(utf8.encode(jsonEncode({
+      'autoDropAfterRelease': true,
+      'stagedRepositoryIds': publicationIds,
+      'description': 'jbuild publication',
+    })));
+    final promoteResponse = await promoteRequest.close();
+    await _verifySuccess(promoteResponse,
+        error: 'All artifacts published, but failed to finalize publication');
+    logger.info('Successfully published artifact');
+  }
+
+  Future<void> _login(Uri repoUri, HttpClient client,
+      HttpClientBasicCredentials credentials) async {
+    final loginUri = repoUri.resolve('authentication/login');
+    client.addCredentials(
+        loginUri,
+        'Sonatype Nexus Repository Manager API (specialized auth)',
+        credentials);
+    final loginRequest = await client.getUrl(loginUri);
+    final loginResponse = await loginRequest.close();
+    await _verifySuccess(loginResponse,
+        error: 'Could not login to Maven repository');
+    logger.fine('Successfully logged in to Maven repository');
+  }
+}
+
+Future<Iterable<String>> _parseUploadResponse(
+    HttpClientResponse uploadResponse) async {
+  final dynamic result = await uploadResponse
+      .transform(const Utf8Decoder())
+      .transform(json.decoder)
+      .first;
+  logger.fine(() => 'Maven Repository upload response: $result');
+  final Iterable<String> repositoryUris = result['repositoryUris'];
+  return repositoryUris
+      .map((uri) => Uri.parse(uri).pathSegments)
+      .where((path) => path.isNotEmpty)
+      .map((path) => path.last);
+}
+
+HttpClientBasicCredentials? _mavenCredentials() {
+  final mavenUser = Platform.environment['MAVEN_USER'];
+  final mavenPassword = Platform.environment['MAVEN_PASSWORD'];
+  if (mavenUser != null && mavenPassword != null) {
+    logger.fine('Using MAVEN_USER and MAVEN_PASSWORD for HTTP credentials');
+    return HttpClientBasicCredentials(mavenUser, mavenPassword);
+  }
+  logger.fine('No HTTP credentials provided');
+  return null;
 }
 
 Future<void> _shaFile(File file) async {
@@ -163,7 +250,7 @@ Future<File> _signFile(File file) async {
       _gpgExists = false;
     }
   } else {
-    logger.fine(() => 'Skipping "gpg" signature for $file.');
+    logger.finer(() => 'Skipping "gpg" signature for $file.');
   }
   return file;
 }
@@ -180,3 +267,16 @@ String _pathFor(Artifact artifact, String parent) {
 String _fileFor(Artifact artifact,
         {String qualifier = '', String extension = '.jar'}) =>
     '${artifact.module}-${artifact.version}$qualifier$extension';
+
+Future<void> _verifySuccess(HttpClientResponse response,
+    {required String error}) async {
+  final status = response.statusCode;
+  if (!_successCodes.contains(status)) {
+    logger.fine(() => 'HTTP Request failed with statusCode $status');
+    var body = await response.lines().join('\n');
+    if (body.trim().isEmpty) {
+      body = 'statusCode = $status';
+    }
+    failBuild(reason: '$error: $body');
+  }
+}
